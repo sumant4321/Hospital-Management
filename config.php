972be41e76e6d8447f57d95fb639e8c23a186171
sumant4321 <50990791+sumant4321@users.noreply.github.com>
2021-08-13 09:15:02 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL & ~ E_NOTICE);
+
+$servername = "localhost";
+$username = "id12891733_hkm";
+$password = "Ankityadav@06";
+$db = "id12891733_madbag";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+?>
