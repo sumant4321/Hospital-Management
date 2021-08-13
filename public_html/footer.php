224fe7cd0630ddfdcade92bc89953138120e6a3b
sumant4321 <50990791+sumant4321@users.noreply.github.com>
2021-08-13 09:10:38 +0530

    Add files via upload

---
+++ public_html/footer.php
@@ -0,0 +1,27 @@
+<div class="footer" style="background-color: #276b94;background-image: url('./image/footer.png');">
+        <div class="container">
+        <div class="row">          
+          <div class="col-6 col-md-6 col-lg-6 col-sm-6 copyright">            
+              <p class=" mb-3" style="color: white;font-family:  'Caladea', serif;padding: 100px 0 0 0;">© COPYRIGHT 2020, NIC. ALL RIGHTS RESERVED ®.</p>
+            </div>
+            <div class="col-6 col-md-6 col-lg-6 col-sm-6 contact-us">            
+              <p class="mt-3 mb-3" style="font-family:'Caladea', serif;font-size:22px">contact us</p>
+              <div class="contact-content">
+              <i class="fas fa-map-marker-alt  fa-1x"></i>
+              <span class="ml-2" style="font-family:Cochin;">Sumant kumar Pvt.Ltd.</span><br>
+              <span style="font-family:Cochin;">Janak kishor road ,Kadam kuan Patna-3</span><br>
+              <i class="fas fa-phone-alt fa-1x"></i>
+              <span class="ml-2" style="font-family:Cochin;">+91-7301323083</span><br>
+              <i class="far fa-envelope"></i>
+              <span class="ml-2 " style="font-family:Cochin;"><a href="mailto:"  style="color:white">sumantkumar4321@gmail.com</a></span><br>              
+              <i class="fas fa-mobile-alt"></i>
+              <span class="ml-2" style="font-family:Cochin;">+91-7301323083</span><br>
+              <div class="contact-icon mt-3">
+              <i class="fab fa-facebook" ></i>
+              <i class="fab fa-google-plus"></i>
+              <i class="fab fa-linkedin"></i>
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
